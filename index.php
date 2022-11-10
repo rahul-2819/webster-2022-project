03f67db112623c15968625aa521ffa2c2c806e80
rahul-2819 <97394262+rahul-2819@users.noreply.github.com>
2022-11-11 04:31:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ function function_alert($message) {
 session_start();
 
 ?>
+<!--  -->
 <!-- here goes our html and differnet tags with it  -->
 <!DOCTYPE html>
 <html lang="en">
@@ -30,7 +31,7 @@ session_start();
     <?php
   if($_SESSION==NULL){
 // header("Location:login2.php");
-echo "LOGIN TO USE OUR SERVICES";
+// echo "LOGIN TO USE OUR SERVICES";
 }
 else if($_SESSION!=NULL){
 header("Location:user.php");
@@ -40,7 +41,7 @@ header("Location:user.php");
   <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
-      <a href="login.php"><img src="https://see.fontimg.com/api/renderfont4/VGORe/eyJyIjoiZnMiLCJoIjozMywidyI6MTUwMCwiZnMiOjIyLCJmZ2MiOiIjRkZGRUZFIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/U01JVC1FLVNIT1A/minecraft-ten.png" alt="Minecraft fonts"></a>
+      <!-- <a href="login.php"><img src="https://see.fontimg.com/api/renderfont4/VGORe/eyJyIjoiZnMiLCJoIjozMywidyI6MTUwMCwiZnMiOjIyLCJmZ2MiOiIjRkZGRUZFIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/U01JVC1FLVNIT1A/minecraft-ten.png" alt="Minecraft fonts"></a> -->
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -54,9 +55,13 @@ header("Location:user.php");
             <a class="nav-link" href="profile.php">Profile</a>
           </li> -->
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            my_order
+            <a class="nav-link dropdown-toggle" href="admin_login.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Admin
             </a>
+            </li>
+            <li class="nav-item  ml-25">
+            <a href="login.php">
+              <img style=" margin-left: 425px;" src="https://see.fontimg.com/api/renderfont4/VGORe/eyJyIjoiZnMiLCJoIjozMywidyI6MTUwMCwiZnMiOjIyLCJmZ2MiOiIjRkZGRUZFIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/U01JVC1FLVNIT1A/minecraft-ten.png" alt="Minecraft fonts"></a>
             </li>
         </ul>
         <?php
@@ -66,7 +71,9 @@ header("Location:user.php");
             else if(array_key_exists('login',$_POST)){
               header("Location:login2.php");
             }
+            
             ?>
+            <button style="margin:1%; border-radius:50%;"><a href="cart.php"><i class="fa fa-shopping-cart fa-lg"></i></a></button>
         <form class="d-flex" method="post">
          <button class="form-control me-2" name="signup" >Sign-up</button>
 
@@ -102,18 +109,72 @@ header("Location:user.php");
       </div>
     </div>
   </nav>
-  
-  <!-- here is our main products  -->
-  <!-- this is first one which is main for everyhting  -->
   <div class="container text-center">
   <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 my-2">
+  <!-- now we will display our products -->
+  <?php
+$server="localhost";
+$username="root";
+$password="";
+$database="product";
+$con=mysqli_connect($server,$username,$password,$database);
+if(!$con){
+  die("connection error".mysqli_connect_error());
+}
+if(isset($_GET['addto'])){
+	$sno = $_GET['addto'];
+  $sql="SELECT * FROM `product` WHERE `s.no` = $sno";
+	$result=mysqli_query($con,$sql);
+	
+  $rows=mysqli_fetch_array($result);
+  $pdt_name=$rows['pdt_name'];
+  $pdt_details=$rows['pdt_details'];
+  $nPrice=$rows['nPrice'];
+  $pPrice=$rows['pPrice'];
+  $pdt_img=$rows['pdt_img'];
+	
+$sql="SELECT * FROM `cart` WHERE `pdt_name` LIKE '$pdt_name'";
+$result=mysqli_query($con,$sql);
+$rows=mysqli_num_rows($result);
+if($rows==0){
+$sql="INSERT INTO `product`.`cart` ( `pdt_name`, `pdt_details`, `nPrice`, `pPrice`,`pdt_img`) VALUES ('$pdt_name', '$pdt_details', '$nPrice', '$pPrice','$pdt_img')";
+	// $result = mysqli_query($conn, $sql);
+	if($con->query($sql)==true){
+		echo "<script>alert('This product is successfully added in the cart');</script>";
+    
+	}
+}
+else{
+  echo "<script>alert('This product is already in the cart');</script>";
+}
+	
+  }
+  $server="localhost";
+$username="root";
+$password="";
+$database="product";
+$con=mysqli_connect($server,$username,$password,$database);
+if(!$con){
+  die("connection error".mysqli_connect_error());
+}
+
+$sql="SELECT * FROM `product`";
+$result=mysqli_query($con,$sql);
+$num=1;
+while($rows=mysqli_fetch_assoc($result)){
+  ?>
+  <!-- here is our main products  -->
+  <!-- this is first one which is main for everyhting  -->
+  
+  
     <div class="col">
-      <div class="p-3 border bg-light">Macbook Pro
-        <img src="macbook.jpg
-        " alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
-        <p>PRICE : <s>$500</s> $345</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
+      <div class="p-3 border bg-light"><?php echo $rows['pdt_name']; ?>
+      <img src="<?php echo $rows['pdt_img'];?>" alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
+        <p> PRICE: <s>$<?php echo $rows['nPrice']; ?></s> $<?php echo $rows['pPrice']; ?></p>
+        <br>
+        <p><?php echo $rows['pdt_details'];?></p>
+        <form action="index.php" method="post">
+        <button type="button" class="btn btn-primary btn-sm" name="add" id="<?php echo $rows['s.no']; ?>">
         ADD TO CART
           <i class="fa fa-shopping-cart fa-lg"></i>
         
@@ -125,187 +186,36 @@ header("Location:user.php");
         <i class="fa fa-star"></i>
         <i class="fa fa-star"></i>
 </div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">iPhone-13-pro-max
-        <img src="iphone-13.jpg" alt="" class="img-fluid img-thumbnail   ">
-        <p>PRICE : <s>$600</s> $300</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">Samsung S-20
-        <img src="samsung S-20.jpg" alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
-        <p>PRICE : <s>$200</s> $189</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">Samsung S-21
-        <img src="samsung s-21.jpg" alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
-        <p>PRICE : <s>$300</s> $250</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">iphone 14
-        <img src="iphone14.jpg" alt="" class="img-fluid img-thumbnail mt-1 mb-2 " >
-        <p>PRICE : <s>$350</s> $300</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-   
-  </div>
-</div>
-<!-- here goes second row  -->
-<div class="container text-center">
-  <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 my-2">
-    <div class="col">
-      <div class="p-3 border bg-light">Macbook Pro
-        <img src="macbook-pro.png" alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
-        <p>PRICE : <s>$500</s> $345</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">Macbook Pro
-        <img src="macbook-pro.png" alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
-        <p>PRICE : <s>$500</s> $345</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">Macbook Pro
-        <img src="macbook-pro.png" alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
-        <p>PRICE : <s>$500</s> $345</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">Macbook Pro
-        <img src="macbook-pro.png" alt="" class="img-fluid img-thumbnail mt-1 mb-2 ">
-        <p>PRICE : <s>$500</s> $345</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-<div class="col">
-      <div class="p-3 border bg-light">Macbook Pro
-        <img src="macbook-pro.png" alt="" class="img-fluid img-thumbnail mt-1 mb-4 ">
-        <p>PRICE : <s>$500</s> $345</p>
-        <form action="#" method="post">
-        <button type="button" class="btn btn-primary btn-sm">
-        ADD TO CART
-          <i class="fa fa-shopping-cart fa-lg"></i>
-        
-        </button>
-        </form>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-        <i class="fa fa-star"></i>
-</div>
-</div>
-   
-  </div>
 </div>
 
+<?php
+$num++;
+}
+?>
+<?php
+$con->close();
+?>
+
+
+
+   
+  
+<!-- here goes second row  -->
+
+
+
+
+   
+  
+
  <!-- Footer -->
-<footer class="page-footer font-small cyan darken-3">
+ <footer class="page-footer w-100 font-small cyan darken-3 mt-5">
 
 <!-- Footer Elements -->
 
 
 <!-- Copyright -->
-<div class="footer-copyright text-center  bg-dark text-light">
+<div class="footer-copyright text-center  bg-grey text-dark">
 <a href="#" style="padding:2%;
 text-decoration:none;">
 <i class="fa fa-facebook fa-2x "></i>
@@ -330,7 +240,7 @@ text-decoration:none;">
 <br>
 
 
-© 2022 Copyright
+© 2022 Copyright:
   <p>Team RNA</p>
 </div>
 <!-- Copyright -->
@@ -340,3 +250,24 @@ text-decoration:none;">
   
 </body>
 </html>
+<script>
+  
+  add = document.getElementsByClassName('btn btn-primary btn-sm');
+    Array.from(add).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        
+        sno = e.target.id;
+
+       if(confirm("you wannna to add product in cart")) {
+       console.log("yes");
+          window.location = `/webster/index.php?addto=${sno}`;
+          // TODO: Create a form and use post request to submit a form
+       }
+       else{
+        console.log("no");
+       }
+       
+      })
+    })
+</script>
+
