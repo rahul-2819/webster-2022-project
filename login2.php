a458a27539818fe693014d069ee7eb05399ffc46
rahul-2819 <97394262+rahul-2819@users.noreply.github.com>
2022-11-11 04:37:19 +0530

    Update login2.php

--- login2.php
+++ login2.php
@@ -47,7 +47,7 @@ $con->close();
         <div class="main">
             <!-- this is main content -->
             <div class="wrapper"> 
-            <a href="login.php"><img src="https://see.fontimg.com/api/renderfont4/VGORe/eyJyIjoiZnMiLCJoIjozMywidyI6MTUwMCwiZnMiOjIyLCJmZ2MiOiIjRkZGRUZFIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/U01JVC1FLVNIT1A/minecraft-ten.png" alt="Minecraft fonts"></a>
+            <a href="login2.php"><img src="https://see.fontimg.com/api/renderfont4/VGORe/eyJyIjoiZnMiLCJoIjozMywidyI6MTUwMCwiZnMiOjIyLCJmZ2MiOiIjRkZGRUZFIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/U01JVC1FLVNIT1A/minecraft-ten.png" alt="Minecraft fonts"></a>
                 <form action="login2.php" method="post">
                     <input class="input1" type="email" class="input" name="email"  id="name" placeholder="Email" required="required" >
                     <input class ="input2" type="password" class="input" name="password" id="password" placeholder="Password" required="required" >
